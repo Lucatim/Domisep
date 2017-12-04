da3a413925b2e1f3d57c067a666f1ed0a8a70040
Lucatim <queant.lucas@gmail.com>
2017-12-04 09:44:48 +0100

    Connexion media queries large desktop

--- view/base/header.php
+++ view/base/header.php
@@ -9,6 +9,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <!-- Definition of the viewport for a mobile screen -->
@@ -23,27 +24,36 @@
         }
     }
     else{
-        echo ('<link rel="stylesheet" href="assets/css/index.css" />');
+        echo ('<link rel="stylesheet" href="../assets/css/index.css" />');
     }
 
     ?>
 
-<<<<<<< HEAD
-
-    <link rel="stylesheet" href="assets/css/screen_large_desktop.css" media="(min-width: 992px)"/>
-    <link rel="stylesheet" href="assets/css/screen_desktop.css" media="(min-width: 768px) and (max-width: 992px)"/>
-    <link rel="stylesheet" href="assets/css/screen_tablet.css" media="(min-width: 480px) and (max-width: 768px)"/>
-    <link rel="stylesheet" href="assets/css/screen_phone.css" media="(max-width: 480px)"/>
-=======
-    <link rel="stylesheet" href="../assets/css/index.css" />
     <link rel="stylesheet" href="../assets/css/screen_large_desktop.css" media="(min-width: 992px)"/>
     <link rel="stylesheet" href="../assets/css/screen_desktop.css" media="(min-width: 768px) and (max-width: 992px)"/>
     <link rel="stylesheet" href="../assets/css/screen_tablet.css" media="(min-width: 480px) and (max-width: 768px)"/>
     <link rel="stylesheet" href="../assets/css/screen_phone.css" media="(max-width: 480px)"/>
->>>>>>> origin/master
+
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
+    <link rel="apple-touch-icon" sizes="57x57" href="../../view/assets/images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../../view/assets/images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../view/assets/images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../view/assets/images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../view/assets/images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../../view/assets/images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../../view/assets/images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../../view/assets/images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../view/assets/images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="../../view/assets/images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../view/assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../view/assets/images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../view/assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../../view/assets/images/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
     <title>Domisep - Accueil</title>
 </head>
@@ -90,7 +100,6 @@ else{
 }
 
 ?>
-
 
 <div id="slide_accueil">
     <div id="slide_circle">
