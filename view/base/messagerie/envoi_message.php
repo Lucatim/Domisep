9b2a686e2bfc10cd6358e59d3f7170c4472366d7
vanke <vankevinsuy@gmail.com>
2017-12-29 18:38:46 +0100

    messagerie

---
+++ view/base/messagerie/envoi_message.php
@@ -0,0 +1,37 @@
+<?php
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=domisep;charset=utf8', 'root', '');
+
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+
+
+$req = $bdd->prepare('INSERT INTO mail(recipient , sender , num_client , subject , mess ) 
+                               VALUES(:recipient , :sender , :num_client , :subject , :mess)');
+
+
+$req->execute(array(
+    'recipient'=> $_POST['recipient'],
+    'sender' => $_POST['sender'],
+    'num_client' => $_POST['num_client'],
+
+    'subject'  => $_POST['subject'],
+    'mess' => $_POST['mess']
+    ));
+
+
+
+header('Location: http://localhost/Domisep/index_mvc.php?target=messagerie#');
+
+
+
+
+
+?>
--- view/base/messagerie/messagerie.php
+++ view/base/messagerie/messagerie.php
@@ -94,7 +94,24 @@
 
             <div class="groupe_bouton_vert">
                 <div class="bouton_vert">
-                    <a href="#"><i class="material-icons">message</i>Nouveau</a>
+                    <a id="myBtn"><i class="material-icons">message</i>Nouveau message</a>
+                    <div id="myModal" class="modal">
+                        <div class="modal-content">
+                            <span class="close">&times;</span>
+
+                            <h2>Enter votre message</h2>
+
+                            <form action="view/base/messagerie/envoi_message.php" method="post">
+                                <label>Envoyer à : <input type="text" name="recipeient" ></label><br>
+                                <label>De : <input type="text" name="sender" ></label><br>
+                                <label>Numéros client : <input type="text" name="num_client" ></label><br>
+                                <label>sujet : <input type="text" name="subject" ></label><br>
+                                <label>message : <input type="textarea" name="mess" ></label><br>
+
+                                <input type="submit" name="envoi" value="envoyer">
+                            </form>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="bouton_vert">
@@ -108,6 +125,41 @@
                 <div class="bouton_vert">
                     <a href="../utilisateur/facture.html"><i class="material-icons">delete</i>Corbeille</a>
                 </div>
+
+
+
+                <script>
+                // Get the modal
+                var modal = document.getElementById('myModal');
+
+                // Get the button that opens the modal
+                var btn = document.getElementById("myBtn");
+
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+                // When the user clicks the button, open the modal
+                btn.onclick = function() {
+                    modal.style.display = "block";
+                }
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() {
+                    modal.style.display = "none";
+                }
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+                </script>
+
+
+
+
+
             </div>
         </div>
     </div>
